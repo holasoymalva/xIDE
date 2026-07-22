e6cd76603ac5903458cb1eebf0c6491a58d00951
Leon Martin <alquimista3301@gmail.com>
2026-07-22 14:06:28 -0600

    Initial Commit

---
+++ xIDE/xIDEApp.swift
@@ -0,0 +1,17 @@
+//
+//  xIDEApp.swift
+//  xIDE
+//
+//  Created by Malva on 22/07/26.
+//
+
+import SwiftUI
+
+@main
+struct xIDEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
